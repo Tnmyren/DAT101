881f298a6fae82e26db7e3549e5c9709beea7c17
Tnmyren <tnmyren55@gmail.com>
2025-09-06 18:32:55 +0200

    Ferdig med oppgave 2.1 og 2.2

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -3,17 +3,14 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-
-    
-      let svar = 2 + 3 * (2-4)*6;
-      let ikke_svar = 2 + 3 *2-4*6;
-      let utrykk1 = "2 + 3 * (2-4)*6 = " ;
-      let utrykk2 = "2 + 3 *2-4*6 = ";
+let svar = 2 + 3 * (2-4)*6;
+let ikke_svar = 2 + 3 *2-4*6;
+let utrykk1 = "2 + 3 * (2-4)*6 = " ;
+let utrykk2 = "2 + 3 *2-4*6 = ";
 printOut(utrykk1);
 printOut(svar);
 printOut(utrykk2);
 printOut(ikke_svar);
-
 
 printOut(newLine);
 
@@ -25,44 +22,82 @@ printOut(inches.toFixed(2));
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let days = 3;
+let hours = 12;
+let minnutes = 14;
+let sek = 45;
+let convert = (sek / 60) + minnutes + hours * 60 + days * 24 * 60;
+printOut(convert);
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let min = 6322.52;
+let convertsek = min * 60;
+let converthours = min / 60;
+let convertdays = converthours / 24;
+printOut("Sek: " +convertsek.toFixed(2));
+printOut("Minutes: " + min);
+printOut("Hours: " + converthours.toFixed(2));
+printOut("Days: " + convertdays.toFixed(2));
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let rate = 8.6 / 76;
+let nok = 56;
+let valuta = nok * rate;
+let omvendt = valuta / rate;
+
+printOut(valuta.toFixed(0) + " USD");
+printOut(omvendt.toFixed(0) + " Kroner");
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let tekst = "There is much between heaven and earth that we do not understand.";
+
+printOut(tekst.length);
+printOut(tekst.charAt(19));
+printOut(tekst.substring(35, 43));
+printOut(tekst.indexOf("earth"));
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let compare1 = 5 > 3;
+let compare2 = 7 >= 7;
+let compare3 = "a" > "b";
+let compare4 = 1 < "a";
+let compare5 = 2500 < "abc";
+let compare6 = "arnee" != "Thomas";
+let compare7 = 2 === 5;
+let compare8 = "abcd" > "bcd";
+
+console.log(compare1,compare2,compare3,compare4,compare5,compare6,compare7,compare8);
+//printOut(compare1,compare2,compare3,compare4,compare5,compare6,compare7,compare8);
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let x = Number("254")
+let z = Number("5723")
+let y = parseInt("25 kroner")
+printOut(x);
+printOut(z);
+printOut(y);
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let minimum = 1;
+let maximum = 360;
+let random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+printOut(random);
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let rest = 131 % 7;
+let weeks = Math.floor(131 / 7);
+printOut(weeks + " weeks");
+printOut(rest + " days");
 printOut(newLine);
 
 
