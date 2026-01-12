2bebc15bab37d848c20ef9c089a38d63442283fb
Thomas Myren <tnmyren55@gmail.com>
2026-01-12 10:26:21 +0100

    ground

--- module-5/Task_5-1_FlappyBird/background.js
+++ module-5/Task_5-1_FlappyBird/background.js
@@ -6,10 +6,14 @@ export class TBackGround{
     #SpriteBackGround;
     #SpriteGround;
     constructor(aSpcvs, aSPI){
-        this.#SpriteBackGround = new TSprite(aSpcvs, aSPI.background,0,0)
+        this.#SpriteBackGround = new TSprite(aSpcvs, aSPI.background,0,0);
+        this.#SpriteGround = new TSprite(aSpcvs, aSPI.ground, 0, 400);
     }
 
     draw(){
         this.#SpriteBackGround.draw();
+        this.#SpriteGround.draw();
     }
+
 }
+
