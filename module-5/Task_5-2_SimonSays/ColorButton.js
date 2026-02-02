33c22915c4812ab0d17d441b93dbf954611fe48c
Thomas Myren <tnmyren55@gmail.com>
2026-02-02 11:24:54 +0100

    made sound

--- module-5/Task_5-2_SimonSays/ColorButton.js
+++ module-5/Task_5-2_SimonSays/ColorButton.js
@@ -1,16 +1,18 @@
 "use strict";
 
 import {TSpriteButton} from "libSprite";
-
+import {EOctave, Notes, TSoundWave, ENoteName} from "libSound"
 
 export class TColorButton extends TSpriteButton {
     #dst
     #Gamecenter
+    #sound
 constructor(aSpcvs, aSPI, aGameBoardCenter){
 
     super(aSpcvs, aSPI, aSPI.dst.x, aSPI.dst.y);
     this.#dst = aSPI.dst
     this.#Gamecenter = aGameBoardCenter;
+    this.#sound = null;
 
 }
 isMouseOver(aMousePos){
@@ -30,10 +32,27 @@ if (isover){
 onMouseDown(){
     //no need to call super
     this.index = 1;
+
+    if (this.#sound){
+        this.#sound.play();
+    }
 }
 onMouseUp(){
     this.index = 0;
+    if (this.#sound) {
+        this.#sound.stop();
+    }
 }
 
+createSound(aIndex){
+    if (aIndex === 0) {
+        this.#sound = new TSoundWave(EOctave.Octave4, ENoteName.A)
+    } else   if (aIndex === 1) {
+        this.#sound = new TSoundWave(EOctave.Octave4, ENoteName.C)
+    } else   if (aIndex === 2) {
+        this.#sound = new TSoundWave(EOctave.Octave4, ENoteName.E)
+     } else   if (aIndex === 3) {
+        this.#sound = new TSoundWave(EOctave.Octave4, ENoteName.G)
 
-}
+
+}}}
--- module-5/Task_5-2_SimonSays/gameBoard.js
+++ module-5/Task_5-2_SimonSays/gameBoard.js
@@ -1,31 +1,68 @@
 "use strict";
 
-import {TSprite} from "libSprite"
-import {TPoint} from "lib2d"
+import {TSprite, TSpriteButton} from "libSprite"
+import {TPoint, TCircle } from "lib2d"
 import { TColorButton } from "./ColorButton.js";
+import {activateAudioContext} from "libSound"
 
 export class TGameBoard extends TSprite{
 
     #colorButtons; 
+    #startgame;
+    #IsSoundEnabled
 constructor(aSpcvs, aSPI){
 
     super(aSpcvs, aSPI.Background, 0, 0);
     const center =  new TPoint(aSPI.Background.width / 2, aSPI.Background.height / 2)
     this.#colorButtons = [
-        new TColorButton(aSpcvs, aSPI.ButtonYellow, center),
+        new TColorButton(aSpcvs, aSPI.ButtonYellow, center),    
         new TColorButton(aSpcvs, aSPI.ButtonRed, center),
         new TColorButton(aSpcvs, aSPI.ButtonBlue, center),
         new TColorButton(aSpcvs, aSPI.ButtonGreen, center),
     ]
 
+    this.#startgame = new TSpriteButton(aSpcvs, aSPI.ButtonStartEnd, aSPI.Background.width / 4, aSPI.Background.height / 4)
+    this.#startgame.onClick = this.#gameinfoclick.bind(this)
+    this.#disablecolorclick(true);
+    this.#IsSoundEnabled = false;
 }
+
+
 
 draw(){
     super.draw();
     for(let i = 0; i < this.#colorButtons.length; i++) {
     const ColorButton = this.#colorButtons[i];
     ColorButton.draw() 
+    this.#startgame.draw()
 }}
+
+#disablecolorclick(aDisable){
+    for(let i = 0; i < this.#colorButtons.length; i++) {
+        const ColorButton = this.#colorButtons[i];
+         ColorButton.disabled = aDisable;
+    }
+}
+
+#gameinfoclick(){
+    this.#startgame.disable = true
+    this.#startgame.hidden = true
+    this.#disablecolorclick(false);
+    if (this.#IsSoundEnabled === false) {
+        activateAudioContext();
+        this.#IsSoundEnabled = true;
+        for(let i = 0; i < this.#colorButtons.length; i++) {
+            const ColorButton = this.#colorButtons[i];
+            ColorButton.createSound(i)
+        }
+    }
+}
+changesound(){
+    if (this.#colorButtons = aSPI.ButtonYellow){
+        createSound(2)
+        console.log(createSound(aIndex))
+    }
+}
 
 
 
